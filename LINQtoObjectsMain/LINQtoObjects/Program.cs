9df2cef7d8c44935a2e5332db88e9401ee0efa3e
erjan123 <erjan123@yahoo.com>
2018-12-17 17:51:21 +0200

    Added 5 Linq Projects

---
+++ LINQtoObjectsMain/LINQtoObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQtoObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
